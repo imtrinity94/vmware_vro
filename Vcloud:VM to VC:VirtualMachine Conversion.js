108ef68b5b1e681e4c86c4bfee2f255a1e4bbd85
Mayank Goyal <7029361+imtrinity94@users.noreply.github.com>
2019-11-05 15:16:23 +0530

    Create Vcloud:VM to VC:VirtualMachine Conversion.js
    Vcloud:VM to VC:VirtualMachine Conversion
    VclVM to VcVirtualMachine Conversion (sciptable object)

---
+++ Vcloud:VM to VC:VirtualMachine Conversion.js
@@ -0,0 +1,19 @@
+//Input vCloudVM (Vcloud:VM)
+//Output VCVM (VC:VirtualMachine)
+
+var vmId = vCloudVM.id.split(":")[3];
+var XPath = "xpath:name[contains(.,('" + vmId + "'))]";
+var vCenterServer = System.getModule("org.telus.xavient.util").getPluginObject("VC:SdkConnection",["isLocal","isPrimary"]);
+var allVMs = vCenterServer.getAllVirtualMachines(null, XPath);
+if (allVMs.length != 0) {
+    for each(var vm in allVMs) {
+        VCVM = vm;
+        var VM_NAME = vm.name;
+        System.log("[Info]: Found Virtual Machine in vCenter: " + VCVM.name);
+        break;
+    }
+} else {
+    System.warn("ERROR: VM Not Found with the id: '" + vmId + "'");
+    throw "ERROR: VM Not Found with the id: '" + vmId + "'";
+}
+	
