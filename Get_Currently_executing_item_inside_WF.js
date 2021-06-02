424b392e195c399de822ae8dfadc9e11d0cca307
Mayank Goyal <7029361+imtrinity94@users.noreply.github.com>
2021-06-02 20:09:16 +0530

    Create Get_Currently_executing_item_inside_WF.js

---
+++ Get_Currently_executing_item_inside_WF.js
@@ -0,0 +1,4 @@
+System.currentWorkflowItem().getDisplayName()  // get "Rename a VM"
+System.currentWorkflowItem().getName()    // get the Item0
+
+//Seems to be working in vRO 8.x and not in earlier versions. But still you can try for yourself.
