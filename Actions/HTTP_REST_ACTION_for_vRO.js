1a2269e357c32be28e563bf06213bdadfae2f45f
Mayank Goyal <imtrinity94@gmail.com>
2022-03-21 08:54:24 +0530

    Update HTTP_REST_ACTION_for_vRO.js

--- Actions/HTTP_REST_ACTION_for_vRO.js
+++ Actions/HTTP_REST_ACTION_for_vRO.js
@@ -1,6 +1,5 @@
 /**
  * Creates and returns an instance of the HttpClient object.
- * @author Mayank Goyal <mayankgoyalmax@gmail.com>
  * @version 1.0.0
  * @function httpClient
  * @returns {*} An instance of the HttpClient object.
