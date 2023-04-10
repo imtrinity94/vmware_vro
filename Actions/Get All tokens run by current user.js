bc34606e24943deb2fe031989c387669f427ec00
Mayank Goyal <imtrinity94@gmail.com>
2023-04-10 15:35:54 +0530

    Update Get All tokens run by current user.js

--- Actions/Get All tokens run by current user.js
+++ Actions/Get All tokens run by current user.js
@@ -1,7 +1,7 @@
 var allTokens = Server.findAllForType('WorkflowToken');
 var currentUser = Server.getCredential().username;
 var res = [];
-for(var i = 0; i<res.length; i++){
+for(var i = 0; i<allTokens.length; i++){
 	if(allTokens[i].runningUserName == currentUser){
 	  res.push(allTokens[i]);
 	}
